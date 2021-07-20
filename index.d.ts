a381f86a3ecef9b5e1dc25ff2d1b2358dc8a85ab
n0n <martti@avalah.ee>
2021-07-20 20:05:19 +0300

    Add: ts index

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+export function breadth(rootNode: any, options: any): void;
+export function breadthSync(rootNode: any, options: any): any;
+export function depth(rootNode: any, options: any): void;
+export function depthSync(rootNode: any, options: any): any;
+export function recursive(rootNode: any, options: any): void;
+export function recursiveSync(rootNode: any, options: any): any;
+
